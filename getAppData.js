32a7cae82e623e825e1fc6be5397860c575b0995
Dneif <dneifiend@gmail.com>
2023-05-26 17:38:43 +0900

    두비리

--- getAppData.js
+++ getAppData.js
@@ -3,6 +3,7 @@ import path from "path"
 import fs from "fs"
 
 // 녹스기준 작동함
+// adb connect 127.0.0.1:62001
 // adb -s 127.0.0.1:62001 pull /data/data/com.digitalextremes.warframenexus/app_appdata
 
 
